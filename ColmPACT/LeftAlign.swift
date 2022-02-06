2e44c389e58f61366391e3300c2435c3f189e806
Kamryn Hines <kamrynhines@kamryns-air.myfiosgateway.com>
2022-02-05 23:46:38 -0500

    final functionality update

--- ColmPACT/LeftAlign.swift
+++ ColmPACT/LeftAlign.swift
@@ -5,43 +5,36 @@
 //  Created by Jaraad Hines on 2/5/22.
 //
 
-func leftAlign(text: String, columns: Int){
+func leftAlign(text: String, columns: Int) {
     var value_index: Int = 0;
-    //setting up every value to have an index value
     let arr = text.split(separator: " ")
-    for val in arr{
+    
+    //iterate through arr
+    for val in arr {
+        //get length of longest value
         if let max = arr.max(by: {$1.count > $0.count}) {
             let longest_value = max.count
-            let currentValue = arr[value_index]
-            let nextValue = arr[value_index + 1]
-    //        let modulus_value = currentValue.count % nextValue.count
-    //        let dynamic_spacing_int : Double = 3.5
-            let dynamic_spacing_int = 5-(columns%5)
-            let dynamic_spacing = String(repeating: " ", count: dynamic_spacing_int)
-            let tabs = String(repeating: "\t", count: 2)
-            print(dynamic_spacing+val+dynamic_spacing, terminator: " ")
-
-
-    //        if (currentValue.count == longest_value) {
-    //            let spacing = String(repeating:"_", count: dynamic_spacing)
-    //            //print our words
-    //            print(spacing+val, terminator: "")
-    //        } else {
-    //            let spacing = String(repeating:"_", count: longest_value + 2)
-    //            //print our words
-    //            print(spacing+val, terminator: "")
-    //        }
-
-        //increment value_index with every val iteration
+            
+            //core computation:
+            //leftAlign spacing for vals and printing vals
+            if (val.count < longest_value) {
+                let spacing_amount = longest_value - val.count
+                let spacing = String(repeating: " ", count: spacing_amount)
+                print(val+spacing, terminator: " ")
+            } else {
+                //longest value spacing
+                let spacing = ""
+                print(val+spacing, terminator: " ")
+            }
+            //increment value_index with every val iteration
             value_index += 1
-
-        //set up our columns using value index to place new line in desired place, equivalent to making columns
+            
+            //print new line for columns
             if (value_index == columns) {
                 print("\n")
                 value_index = 0;
             }
         }
-
     }
 }
 
