84db64bf05b4c7b080f0198edf8b5331f049f6db
Kamryn Hines <kamrynhines@kamryns-air.myfiosgateway.com>
2022-02-09 09:55:30 -0500

    function cleanup

--- ColmPACT/LeftAlign.swift
+++ ColmPACT/LeftAlign.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
-func leftAlign(text: String, columns: Int) {
+func leftAlign(text: String, columns: Int) -> String {
     var value_index: Int = 0;
     let arr = text.split(separator: " ")
+    var value = ""
+    var multilineString = """
+    """
     
     //iterate through arr
     for val in arr {
@@ -22,21 +25,24 @@ func leftAlign(text: String, columns: Int) {
             if (val.count < longest_value) {
                 let spacing_amount = longest_value - val.count
                 let spacing = String(repeating: " ", count: spacing_amount)
-                print(val+spacing, terminator: " ")
+                value = val+spacing+"  "
+                multilineString.append(value)
             } else {
                 //longest value spacing
                 let spacing = String(repeating: "", count: 0)
-                print(val+spacing, terminator: " ")
+                value = val+spacing+"  "
+                multilineString.append(value)
             }
-            
             //increment value_index with every val iteration
             value_index += 1
             
             //print new line for columns
             if (value_index == columns) {
-                print("\n")
+                value = "\n"
+                multilineString.append(value)
                 value_index = 0;
             }
         }
     }
+    return(multilineString)
 }
--- ColmPACT/MainView.swift
+++ ColmPACT/MainView.swift
@@ -11,8 +11,7 @@ struct MainView: View {
     
     @State private var inputText = String()
     @State private var inputColumns = Int()
-    @State var submit = false
-    @State private var isShowingModal = false
+    @State var changeText: String = ""
     
     var body: some View {
         NavigationView {
@@ -23,18 +22,22 @@ struct MainView: View {
                     TextField("How Many Columns?", value: $inputColumns, format: .number)
                         .textFieldStyle(.roundedBorder)
                 }
+                TextEditor(text:$changeText)
                 Button(action: {
-                    self.submit.toggle()}) {
-                            Text("Submit")
-                        }
+                    self.changeText = "\(leftAlign(text: inputText, columns: inputColumns))"
+                }) {
+                    Text("Click To View Your Formatted Sentence!")
+                }
             }
-            .onSubmit(of: .text) {
-                            leftAlign(text: inputText, columns: inputColumns)
-                        }
             .navigationBarTitle("Column Formatter")
         }
     }
 }
+//get my TextEditor View to contain the results of my (leftAlign) function
+//the TextEditor view has "text" as it's parameter; any others?
+//switch statement for every value because if else is not conducive for storing val in value
+//now the value that has our result is our multilineString
+
 
 struct MainViews: PreviewProvider {
     static var previews: some View {
